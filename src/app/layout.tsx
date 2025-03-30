398bf1549c8f064fa4451f9d7e79387d83a7c7e1
Matheus <matheus.felizardo2@gmail.com>
2025-03-30 21:06:59 +0100

    component: sidebarmenu

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/context/AuthProvider";
+import SidebarMenu from "@/components/SidebarMenu";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <AuthProvider>
-        <body className={`${inter.className} antialiased`}>{children}</body>
+        <body className={`${inter.className} antialiased`}>
+          <div className="flex">
+            <SidebarMenu />
+
+            <div className="flex flex-col min-h-screen">{children}</div>
+          </div>
+        </body>
       </AuthProvider>
     </html>
   );
---
+++ src/components/SidebarMenu.tsx
@@ -0,0 +1,159 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import { useState } from "react";
+import {
+  LayoutDashboard,
+  Microscope,
+  Brain,
+  Users,
+  FileChartColumn,
+  Settings,
+  CircleUser,
+  LogOut,
+  ChevronLeft,
+} from "lucide-react";
+import Image from "next/image";
+
+type MenuItemProps = {
+  title: string;
+  icon: React.ElementType;
+  url: string;
+  onClick?: () => void;
+  collapsed: boolean;
+};
+
+const menuItems = [
+  { title: "Dashboard", icon: LayoutDashboard, url: "/dashboard" },
+  { title: "Experiments", icon: Microscope, url: "/experiments" },
+  { title: "Trainings", icon: Brain, url: "/trainings" },
+  { title: "Participants", icon: Users, url: "/participants" },
+  { title: "Reports", icon: FileChartColumn, url: "/media" },
+  { title: "Settings", icon: Settings, url: "/settings" },
+];
+
+const configItems = [
+  { title: "Account", icon: CircleUser, url: "/account" },
+  {
+    title: "Logout",
+    icon: LogOut,
+    url: "/logout",
+    onClick: () => console.log("logout"),
+  },
+];
+
+function MenuItem({
+  title,
+  icon: Icon,
+  url,
+  onClick,
+  collapsed,
+}: MenuItemProps) {
+  const pathname = usePathname();
+  const isActive = pathname.startsWith(url);
+
+  const baseClass = `h-10 flex items-center gap-3 p-4 rounded-md transition-colors ${
+    isActive
+      ? "bg-blue-700 text-white font-semibold"
+      : "text-white hover:bg-blue-600"
+  } ${collapsed && "overflow-hidden w-12"}`;
+
+  if (onClick) {
+    return (
+      <button onClick={onClick} className={baseClass + " cursor-pointer"}>
+        <div className="flex w-5">
+          <Icon size={20} />
+        </div>
+        <span
+          className={`flex-1 transition-all ${collapsed ? "w-0" : "w-full"}`}
+        >
+          {title}
+        </span>
+      </button>
+    );
+  }
+
+  return (
+    <Link href={url} className={baseClass}>
+      <div className="flex w-5">
+        <Icon size={20} />
+      </div>
+      <span className={`flex-1 transition-all ${collapsed ? "w-0" : "w-full"}`}>
+        {title}
+      </span>
+    </Link>
+  );
+}
+
+export default function SidebarMenu() {
+  const [collapsed, setCollapsed] = useState(false);
+
+  return (
+    <aside
+      className={`h-screen bg-blue-capekids shadow-lg flex flex-col justify-between transition-all duration-300 ${
+        collapsed ? "w-20" : "w-52"
+      }`}
+    >
+      <div className="p-4 space-y-4">
+        <div className="flex items-center justify-center mb-16">
+          {collapsed ? (
+            <Image
+              src={"/logo-colapsed.svg"}
+              alt="Logo"
+              width={40}
+              height={40}
+              className={"relative left-2"}
+            />
+          ) : (
+            <Image
+              src={"/logo-white.svg"}
+              alt="Logo"
+              width={140}
+              height={40}
+              className="min-w-36"
+            />
+          )}
+        </div>
+        <p
+          className={`text-xs text-white mb-4 font-light ${
+            collapsed ? "px-2" : "px-4"
+          }`}
+        >
+          MENU
+        </p>
+        <ul className="space-y-2">
+          {menuItems.map((item) => (
+            <li key={item.title}>
+              <MenuItem {...item} collapsed={collapsed} />
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="p-4 border-t border-white space-y-2">
+        <ul className="space-y-2">
+          {configItems.map((item) => (
+            <li key={item.title}>
+              <MenuItem {...item} collapsed={collapsed} />
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <button
+        onClick={() => setCollapsed(!collapsed)}
+        className="flex gap-3 w-full px-8 h-10 items-center bg-blue-700 text-white cursor-pointer"
+      >
+        <div className="flex w-5">
+          <ChevronLeft size={24} className={`${collapsed && "rotate-180"} `} />
+        </div>
+        <span
+          className={`transition-all ${collapsed ? "w-0" : "will-change-auto"}`}
+        >
+          {!collapsed && "Collapse"}
+        </span>
+      </button>
+    </aside>
+  );
+}
