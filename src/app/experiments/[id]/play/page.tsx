0d3ba0ac241df311c52eee98c8d2a6d8dc9d8d4e
Matheus <matheus.felizardo2@gmail.com>
2025-05-28 12:24:57 +0100

    experiment result e participant

---
+++ src/app/experiments/[id]/play/page.tsx
@@ -0,0 +1,94 @@
+"use client";
+import CanvasDebugger from "@/modules/canvas/components/CanvasDebugger";
+import CanvasRunner from "@/modules/canvas/components/CanvasRunner";
+import { useCanvasStore } from "@/modules/canvas/store/useCanvasStore";
+import { useAuthStore } from "@/stores/auth/useAuthStore";
+import { useResultsStore } from "@/stores/results/useResultsStore";
+import { API } from "@/utils/api";
+import { compileTimeline } from "@/utils/functions";
+import { TimelineStep } from "@shared/timeline";
+import { useParams } from "next/navigation";
+import React, { useEffect, useState } from "react";
+
+const PlayExperiment = () => {
+  const { authState } = useAuthStore();
+  const searchParams = useParams();
+  const stepId = searchParams.id as string;
+  const [started, setStarted] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [steps, setSteps] = useState<TimelineStep[] | null>(null);
+
+  const compileSteps = async () => {
+    const request = await fetch(API.EXPERIMENT_BY_ID(stepId || ""), {
+      headers: {
+        Authorization: `Bearer ${authState.token}`,
+      },
+    });
+    const response = await request.json();
+
+    if (response.error) {
+      console.error("Error fetching experiment:", response.error);
+      setLoading(false);
+      return;
+    }
+    const compiledSteps = await compileTimeline(response.data.timeline.steps);
+    setSteps(compiledSteps);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    if (!authState.user) return;
+    compileSteps();
+  }, [authState]);
+
+  const handleStart = async (e) => {
+    const el = document.documentElement;
+    if (el.requestFullscreen) {
+      try {
+        await el.requestFullscreen();
+      } catch (err) {
+        console.warn("Fullscreen não permitido:", err);
+      }
+    }
+    setTimeout(() => {
+      setStarted(true);
+    }, 500);
+  };
+
+  return (
+    <>
+      <div className="relative w-screen h-screen overflow-hidden bg-white">
+        <div className="relative w-full h-full overflow-hidden">
+          {!started && <div className="absolute inset-0 bg-black z-50" />}
+
+          {started && steps && (
+            <>
+              <CanvasRunner steps={steps} started={started} />
+            </>
+          )}
+        </div>
+
+        <div
+          onClick={handleStart}
+          className={`absolute inset-0 z-50 bg-black text-white flex flex-col items-center justify-center transition-opacity duration-500 ${
+            started ? "opacity-0 pointer-events-none" : "opacity-100"
+          }`}
+        >
+          {loading ? (
+            <>
+              <div className="w-10 h-10 border-4 border-white border-t-transparent rounded-full animate-spin mb-4" />
+              <p className="text-sm text-gray-300">Loading...</p>
+            </>
+          ) : (
+            <>
+              <h1 className="text-2xl mb-6">Clique na tela para iniciar</h1>
+            </>
+          )}
+        </div>
+      </div>
+      <div id="feedback-overlay-root" />
+    </>
+  );
+};
+
+export default PlayExperiment;
